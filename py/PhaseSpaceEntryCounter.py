d67ac189ebac45046dc35a9667937a026c64b5ff
lb8075 <lb8075@bp1-login01.data.bp.acrc.priv>
2020-08-30 22:30:05 +0100

    added 10x10 electron phs3 files for propagationg of jaw scattered electrons through sup.Si

---
+++ py/PhaseSpaceEntryCounter.py
@@ -0,0 +1,19 @@
+import numpy as np
+from ROOT import TFile, TTree
+# entries=np.array([])
+
+stringy=""
+
+for i in range (1,401):
+	filename="/work/lb8075/PhaseSpaces/PhS2Elec/output2e9primaries-lana2-PhS-e_nobias"+str(i)+".root"
+	rfile=TFile(filename)
+	rtree=rfile.Get("PhaseSpace")
+	ent=rtree.GetEntries()
+	# entries=np.append(entries,ent)
+	# print(ent)
+	if i==1:
+		stringy=stringy+"["+str(ent)
+	else:
+		stringy=stringy+" "+str(ent)
+stringy=stringy+"]"
+print(stringy)
